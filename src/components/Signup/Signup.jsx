c1ead2480a1448282f4cfeed0d977c8349755c7f
Farooq dad khan <farooqdaadkhan@gmail.com>
2022-10-28 02:05:30 -0700

    removed loading from signup

--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -100,7 +100,6 @@ const Signup = () => {
 
         <div>
             <Header />
-            {isLoading && "Loading.."}
             <div className="container mt-4">
                 <div className="row justify-content-center">
                     <div className="col-md-6 col-lg-6">
